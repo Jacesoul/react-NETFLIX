925f3568533ad3b86b5f0a101062a861fe2e28ec
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-03 14:36:41 +0900

    Header SetUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/tv">
+          <Tv></Tv>
+        </Route>
+        <Route path="/search">
+          <Search></Search>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
