234c56109c19e461a62b4278c083e756348b7fd7
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-03 23:10:50 +0900

    Banner 및 Title, Overview 생성

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "e234c8f082fd3b21c327e654e30ec72d";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMoive {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximun: string;
+    minimun: string;
+  };
+  page: number;
+  results: IMoive[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(
     `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=kr`
